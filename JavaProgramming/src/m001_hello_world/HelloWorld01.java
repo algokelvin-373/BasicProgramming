c85c13bbe97b4c122c66ec7814ceaaef74e96b2a
kelvin373-ht <kelvinht373@gmail.com>
2021-09-26 22:05:44 +0700

    [Java] M001_Hello_World

---
+++ JavaProgramming/src/m001_hello_world/HelloWorld01.java
@@ -0,0 +1,7 @@
+package m001_hello_world;
+
+public class HelloWorld01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World Java Programming");
+    }
+}
---
+++ JavaProgramming/src/m001_hello_world/HelloWorld02.java
@@ -0,0 +1,10 @@
+package m001_hello_world;
+
+public class HelloWorld02 {
+    public static void main(String[] args) {
+        System.out.println("Java Programming Basic");
+        System.out.println("Number - 12345678");
+        System.out.println("String - I'm Algokelvin");
+        System.out.println("Focus on Finding Solutions");
+    }
+}
