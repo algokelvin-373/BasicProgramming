aa157eeb9e5298a9c3c045d68fccb741dc8b9115
kelvin373-ht <kelvinht373@gmail.com>
2021-09-26 22:29:37 +0700

    [Go] M001_Hello_World

---
+++ GoProgramming/src/main/m001_hello_world.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Go Programming Basic")
+	fmt.Println("Number - 123456789")
+	fmt.Println("String - I'm Algokelvin")
+	fmt.Println("Focus on finding Solutions")
+}
