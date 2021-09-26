1962eff635e39a548b015a1846cd16bbf5f16990
kelvin373-ht <kelvinht373@gmail.com>
2021-09-26 22:22:15 +0700

    [C++] M001_Hello_World

---
+++ C++_Programming/m001_hello_world/HelloWorld02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "C++ Programming Basic";
+  cout << "Number - 123456789";
+  cout << "String - I'm Algokelvin";
+  cout << "Focus on finding Solutions";
+  return 0;
+}
