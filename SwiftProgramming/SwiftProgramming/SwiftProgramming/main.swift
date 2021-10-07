9ec786982521db007f72817016c4751f08a0107c
kelvin373-ht <kelvinht373@gmail.com>
2021-10-07 21:02:17 +0700

    [Swift] New Projects

---
+++ SwiftProgramming/SwiftProgramming/SwiftProgramming/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftProgramming
+//
+//  Created by Kelvin HT on 10/7/21.
+//  Copyright © 2021 Kelvin HT. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
