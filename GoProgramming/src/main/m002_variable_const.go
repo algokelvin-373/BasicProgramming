d02bb9f25e8254260378d652e8972991d1109c26
kelvin373-ht <kelvinht373@gmail.com>
2021-10-25 14:09:36 +0700

    [Go] M002_Variable_Const

---
+++ GoProgramming/src/main/m002_variable_const.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const PHI = 3.14
+	const M_Electron = 9.1e-31
+
+	fmt.Println("Value Phi : ", PHI)
+	fmt.Println("Value M_Electron : ", M_Electron)
+}
