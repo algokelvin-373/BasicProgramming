18d9f650a57fbf9235e53cc1612e319bc91aa1b3
kelvin373-ht <kelvinht373@gmail.com>
2021-10-25 10:47:44 +0700

    [Go] M002_Variable_Type - Boolean

--- GoProgramming/src/main/m002_variable_type.go
+++ GoProgramming/src/main/m002_variable_type.go
@@ -7,15 +7,18 @@ func main() {
 	var a int // initialize 'int'
 	var b float32 // initialize 'float32'
 	var c string // initialize 'string'
-	a = 1; b = 3.14; c = "Algokelvin";
+	var x bool // initilialize 'boolean'
+	a = 1; b = 3.14; c = "Algokelvin"; x = true;
 	fmt.Println("a = ", + a) 
 	fmt.Println("b = ", + b)
 	fmt.Println("c = ", c)
+	fmt.Println("x = ", x)
 
 	// Without Initialize Variable
-	var d, e, f = 10, 1.41, "Algokelvin 2"
+	var d, e, f, y = 10, 1.41, "Algokelvin 2", true
 	fmt.Println("d = ", + d) 
 	fmt.Println("e = ", + e)
 	fmt.Println("f = ", f)
+	fmt.Println("y = ", y)
 
 }
