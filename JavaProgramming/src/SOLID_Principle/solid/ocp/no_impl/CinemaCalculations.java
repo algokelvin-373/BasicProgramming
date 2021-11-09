ef52a5e8afa7618c00c4efb57e8ed8f59b580707
algokelvin-373 <kelvinht373@gmail.com>
2021-11-09 12:24:38 +0700

    [Java] SOLID Principles - OCP (No Implement)

---
+++ JavaProgramming/src/SOLID_Principle/solid/ocp/no_impl/CinemaCalculations.java
@@ -0,0 +1,14 @@
+package SOLID_Principle.solid.ocp.no_impl;
+
+public class CinemaCalculations {
+    public double calculateAdminFee(Cinema_No_OCP cinema) {
+        if (cinema instanceof StandardCinema_No_OCP) {
+            return ((StandardCinema_No_OCP) cinema).price * 10 / 100;
+        } else if (cinema instanceof DeluxeCinema_No_OCP) {
+            return ((DeluxeCinema_No_OCP) cinema).price * 12 / 100;
+        } else if (cinema instanceof PremiumCinema_No_OCP) {
+            return ((PremiumCinema_No_OCP) cinema).price * 20 / 100;
+        } else
+            return 0.0;
+    }
+}
