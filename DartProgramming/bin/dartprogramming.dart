b5264456b1c932eeee5adff236020804cae9ddbe
algokelvin-373 <kelvinht373@gmail.com>
2021-11-15 22:38:25 +0700

    [Dart] M001_Hello_World

--- DartProgramming/bin/dartprogramming.dart
+++ DartProgramming/bin/dartprogramming.dart
@@ -1,3 +1,4 @@
 void main() {
   print('Test Program Again');
 }
+//https://exerror.com/remote-support-for-password-authentication-was-removed-on-august-13-2021-please-use-a-personal-access-token-instead/
