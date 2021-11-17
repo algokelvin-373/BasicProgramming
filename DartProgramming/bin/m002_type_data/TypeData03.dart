e0485955fda232c72235695931ef8380bd711727
algokelvin-373 <kelvinht373@gmail.com>
2021-11-17 11:31:26 +0700

    [Dart] TypeData02: Type Data 'num'

---
+++ DartProgramming/bin/m002_type_data/TypeData03.dart
@@ -0,0 +1,10 @@
+//Type Data 'num'
+void main() {
+  int a = 373;
+  double b = 3.14;
+
+  num x = a; // integer
+  print('num x before : ' + x.toString());
+  x = b; // double
+  print('num x after  : ' + x.toString());
+}
