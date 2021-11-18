ee32067ddb5c8fd7690b0aa9ae69abbd727a5435
algokelvin-373 <kelvinht373@gmail.com>
2021-11-18 19:45:04 +0700

    [Dart] Operator04: Operator Relational 1

---
+++ DartProgramming/bin/m004_operator/Operator04.dart
@@ -0,0 +1,11 @@
+//Operator Relational 1
+void main() {
+  int i = 8, j = 7, k = 8, l = 9;
+  print('* Operator Relational : "==" -> Equal to');
+  print('* $i == $j -> ${i == j}');
+  print('* $i == $k -> ${i == k}');
+  print('* ------------------------');
+  print('* Operator Relational : "!=" -> Not Equal to');
+  print('* $i != $j -> ${i != j}');
+  print('* $i != $k -> ${i != k}');
+}
