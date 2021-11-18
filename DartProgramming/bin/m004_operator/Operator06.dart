6c0fd840cb886ffc34825e0354e1df0716a314c3
algokelvin-373 <kelvinht373@gmail.com>
2021-11-18 19:51:47 +0700

    [Dart] Operator06: Operator Relational 3

---
+++ DartProgramming/bin/m004_operator/Operator06.dart
@@ -0,0 +1,13 @@
+//Operator Relational 2
+void main() {
+  int i = 8, j = 7, k = 8, l = 9;
+  print('* Operator Relational : ">=" -> Bigger or equal to');
+  print('* $i >= $j -> ${i > j}');
+  print('* $i >= $k -> ${i >= k}');
+  print('* $i >= $l -> ${i > l}');
+  print('* ------------------------');
+  print('* Operator Relational : "<=" -> Smaller or equal to');
+  print('* $i <= $j -> ${i < j}');
+  print('* $i <= $k -> ${i <= k}');
+  print('* $i <= $l -> ${i < l}');
+}
