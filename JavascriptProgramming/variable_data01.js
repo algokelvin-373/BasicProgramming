803f74f440f6c0137a73a75deda4ad4200712080
algokelvin-373 <kelvinht373@gmail.com>
2021-11-21 22:37:14 +0700

    [Javascript] 016_web_js: variable 'var' repair

--- JavascriptProgramming/variable_data01.js
+++ JavascriptProgramming/variable_data01.js
@@ -3,4 +3,6 @@ function showResultVar() {
     document.getElementById("before").innerHTML = "Before x : "+ x;
     x = 10;
     document.getElementById("after").innerHTML = "After x  : "+ x;
+    var x = 373;
+    document.getElementById("again").innerHTML = "Again x  : "+ x;
 }
