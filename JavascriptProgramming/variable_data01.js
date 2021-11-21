09bd0d3523a3ce73cfa7a839d0fd8b7dc86a8ecc
algokelvin-373 <kelvinht373@gmail.com>
2021-11-21 22:31:49 +0700

    [Javascript] 016_web_js: variable 'var'

---
+++ JavascriptProgramming/variable_data01.js
@@ -0,0 +1,6 @@
+function showResultVar() {
+    var x = 5;
+    document.getElementById("before").innerHTML = "Before x : "+ x;
+    x = 10;
+    document.getElementById("after").innerHTML = "After x  : "+ x;
+}
