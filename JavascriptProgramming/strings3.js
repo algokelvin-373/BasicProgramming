39ab53d6dcc3a8fb8487cb6970906a8149b24873
algokelvin-373 <kelvinht373@gmail.com>
2021-11-30 21:54:19 +0700

    [Javascript] 021_web_js: JS String padding, charAt, charCodeAt, property

---
+++ JavascriptProgramming/strings3.js
@@ -0,0 +1,8 @@
+let x = "3";
+document.getElementById('padStart').innerHTML = x.padStart(4,0)
+document.getElementById('padEnd').innerHTML = x.padEnd(4,0)
+
+let data = "AlgoKelvin"
+document.getElementById('charAt').innerHTML = data.charAt(5)
+document.getElementById('charCodeAt').innerHTML = data.charCodeAt(5)
+document.getElementById('property').innerHTML = data[5]
