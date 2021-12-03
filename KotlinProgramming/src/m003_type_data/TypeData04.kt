889d3af433a8a44f357cc97686b004a63fdd27bd
algokelvin-373 <kelvinht373@gmail.com>
2021-12-03 22:14:49 +0700

    [Kotlin] m003_type_data Part 2

---
+++ KotlinProgramming/src/m003_type_data/TypeData04.kt
@@ -0,0 +1,12 @@
+package m003_type_data
+
+fun main() {
+    println("Type Data 4: Number - Specific variable")
+    val x1: Int? = 10 // Int
+    val x2: Double? = 3.14 // Double
+    val x3: Float? = 9.1f // Float
+    val x4: Double? = 5e+5 // Exponential
+    val x5: Long? = 500L // Long
+    println("$x1 $x2 $x3")
+    println("$x4 $x5")
+}
---
+++ KotlinProgramming/src/m003_type_data/TypeData05.kt
@@ -0,0 +1,8 @@
+package m003_type_data
+
+fun main() {
+    println("Type Data 5: String")
+    val msg1 = "AlgoKelvin" // using val
+    val msg2: String? = "learn Kotlin" // Specific
+    println("$msg1 $msg2")
+}
