84bfa85ff45f4cf577dfa2de413dca292cb31538
algokelvin-373 <kelvinht373@gmail.com>
2022-01-03 20:09:18 +0700

    [Java] 003_05 Switch operator

---
+++ JavaProgramming/src/m003_conditional/SwitchOperator.java
@@ -0,0 +1,26 @@
+package m003_conditional;
+
+import java.util.Scanner;
+
+public class SwitchOperator {
+    public static void main(String[] args) {
+        System.out.println("Card 1 || Card 2 || Card 3");
+
+        //Input keyboard to choose card
+        Scanner input = new Scanner(System.in);
+        System.out.print("Choose card: ");
+        int choose = input.nextInt();
+
+        // Switch Operator
+        switch (choose) {
+            case 1: System.out.println("You Get IDR 5.000");
+                break;
+            case 2: System.out.println("You Get IDR 50.000");
+                break;
+            case 3: System.out.println("You Get IDR 500.000");
+                break;
+            default: System.out.println("Wrong to choose Card");
+                break;
+        }
+    }
+}
