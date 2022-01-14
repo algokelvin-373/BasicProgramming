557b5b2622afdb2119b9bc7e77284d060203f321
algokelvin-373 <kelvinht373@gmail.com>
2022-01-14 23:32:14 +0700

    [Go] Variable_number 01 - 03

---
+++ GoProgramming/src/main/m002_variable_number_02.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Set type variable without "var"
+	d := 10; e := 20; f := 30;
+	fmt.Println("Set type variable without 'var'")
+	fmt.Println("d = ", + d)
+	fmt.Println("e = ", + e)
+	fmt.Println("f = ", + f)
+
+}
+
