1fc1e5c8e3168bc298937184ed892349158f07d5
algokelvin-373 <kelvinht373@gmail.com>
2022-01-16 18:03:07 +0700

    [C++] 003 Input Output

---
+++ C++_Programming/m003_input_output/InputOutput.cpp
@@ -0,0 +1,23 @@
+//
+// Created by AlgoKelvin on 1/16/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int dataNum;
+    string dataStr;
+
+    // Input Data Number and Word
+    cout << "Input Your Number: ";
+    cin >> dataNum;
+    cout << "Input Your Word: ";
+    cin >> dataStr;
+
+    cout << "dataNum = " << dataNum << endl;
+    cout << "dataStr = " << dataStr << endl;
+
+    return 0;
+}
