65172777e9f67ba4cd8f2108912938d0e7b61ef0
algokelvin-373 <kelvinht373@gmail.com>
2022-01-16 20:25:52 +0700

    [C++] 004 Operator 1 - 2

---
+++ C++_Programming/m004_operator/Operator02.cpp
@@ -0,0 +1,21 @@
+//
+// Created by AlgoKelvin on 1/16/22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Arithmetic Operator ( Increment, Decrement )
+    int x = 4, y = 10;
+
+    // x++ and y--
+    cout << "Before: x = " << x << " y = " << y << endl;
+    cout << "After : x = " << x++ << " y = " << y-- << endl;
+
+    // ++x and --y
+    cout << "Before: x = " << x << " y = " << y << endl;
+    cout << "After : x = " << ++x << " y = " << --y << endl;
+
+    return 0;
+}
