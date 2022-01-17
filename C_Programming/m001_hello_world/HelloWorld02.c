e9a29ca21c3d3739872672624b4c9e5f99d1e441
algokelvin-373 <kelvinht373@gmail.com>
2022-01-17 15:17:19 +0700

    [C] 001 Hello World

---
+++ C_Programming/m001_hello_world/HelloWorld02.c
@@ -0,0 +1,13 @@
+//
+// Created by AlgoKelvin on 1/17/22.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("C Programming Basic\n");
+    printf("Number - 12345\n");
+    printf("String - I'm AlgoKelvin\n");
+    printf("Focus on finding Solutions\n");
+    return 0;
+}
