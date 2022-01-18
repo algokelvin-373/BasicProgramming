9951ab10712425e1d77b42dc3a7c7ad1e3427e49
algokelvin-373 <kelvinht373@gmail.com>
2022-01-18 22:08:06 +0700

    [C] 03 Input Output 2

---
+++ C_Programming/m003_input_output/InputOutput02.c
@@ -0,0 +1,20 @@
+//
+// Created by Kelvin HT on 1/18/22.
+//
+
+#include <stdio.h>
+
+int main() {
+
+    // Input String with 'gets'
+    char text[50];
+    printf("\nInput Your Text   : ");
+    gets(text);
+
+    // Output String with 'puts'
+    printf("\nText   : ");
+    puts(text);
+
+    return 0;
+}
+
