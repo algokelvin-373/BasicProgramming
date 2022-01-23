aceb0a92134c1ea8fa041a335340b560f399e8e0
algokelvin-373 <kelvinht373@gmail.com>
2022-01-23 20:02:38 +0700

    [Go] Clean Code

--- GoProgramming/src/main/m002_variable_number_03.go
+++ GoProgramming/src/main/m002_variable_number_03.go
@@ -8,7 +8,6 @@ func main() {
 
 	// Variable With 'var' but not set type variable
 	var g, h, i = 100, 200, 300
-	fmt.Println("Initialize Variable With 'var' But Not Set Variable")
 	fmt.Println("g = ", + g)
 	fmt.Println("h = ", + h)
 	fmt.Println("i = ", + i)
