41aabd57a48328134eff7f46fa522eebf0639fe0
algokelvin-373 <kelvinht373@gmail.com>
2022-02-04 23:36:45 +0700

    [Swift] 05 Type Data Number, Exp, Format Number

---
+++ SwiftProgramming/m005_type_data_number_exp_format/TypeDataExp.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+
+import Cocoa
+
+// 1.25e2 means 1.25 x 10^4, or 12500.0.
+let decExpPositive = 1.25e4
+
+// 1.25e-2 means 1.25 x 10^-4, or 0.000125
+let decExpNegative = 1.25e-4
+
+print(decExpPositive)
+print(decExpNegative)
+
---
+++ SwiftProgramming/m005_type_data_number_exp_format/TypeDataFormatNum.playground/Contents.swift
@@ -0,0 +1,12 @@
+
+
+import Cocoa
+
+// Format Number
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
+print(paddedDouble)
+print(oneMillion)
+print(justOverOneMillion)
