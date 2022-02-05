1def91aca28e940b6a556515c58bae94e4136d72
algokelvin-373 <kelvinht373@gmail.com>
2022-02-05 16:44:05 +0700

    [Swift] 06 Operator

---
+++ SwiftProgramming/m006_operator/OperatorRelational.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+
+import Cocoa
+
+var a = 2 == 2
+var b = 3 < 4
+var c = 2 <= 1
+var d = 8 > 4
+var e = 3 >= 3
+var f = 3 != 3
+
+print(a) // true
+print(b) // true
+print(c) // false
+print(d) // true
+print(e) // true
+print(f) // false
