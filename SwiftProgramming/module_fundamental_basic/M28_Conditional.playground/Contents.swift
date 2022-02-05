2d314bd2885a301c088824025404d6c157e07acf
algokelvin-373 <kelvinht373@gmail.com>
2022-02-05 16:59:41 +0700

    [Swift] 07 Conditional

--- SwiftProgramming/module_fundamental_basic/M28_Conditional.playground/Contents.swift
+++ SwiftProgramming/module_fundamental_basic/M28_Conditional.playground/Contents.swift
@@ -2,26 +2,6 @@ import UIKit
 
 // if, if-else, if-else if-else
 let number = 50
-if (number < 100) {
-    print("number is under 100")
-}
-
-if (number % 5 == 0) {
-    print("\(number) is multiple by 5")
-}
-else {
-    print("\(number) isn't multiple by 5")
-}
-
-if (number % 15 == 0) {
-    print("\(number) is multiple by 15")
-}
-else if (number % 20 == 0) {
-    print("\(number) is multiple by 20")
-}
-else {
-    print("\(number) isn't multiple by 15 or 20")
-}
 
 // switch
 let number02 = 2
