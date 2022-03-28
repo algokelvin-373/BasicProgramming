c6e2af428a0031bcf87a78b3bad85c132957adf1
algokelvin-373 <kelvinht373@gmail.com>
2022-03-28 22:39:01 +0700

    [Kotlin] 11 When Expression

--- KotlinProgramming/src/m010_multi_conditional/Conditonal03.kt
+++ KotlinProgramming/src/m010_multi_conditional/Conditonal03.kt
@@ -4,8 +4,7 @@ fun main() {
     print("Input number: ")
     val a = readLine() // Input from Keyboard
 
-    // Double Conditional (If-Else)
-    val num = a
+    // Multi Conditional
     if (a?.toInt()!! % 2 == 0) {
         println("$a is divided by 2")
     } else if (a.toInt() % 3 == 0) {
