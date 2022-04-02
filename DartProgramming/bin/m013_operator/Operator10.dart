0f62f53a8972522030289490a7e4effaea6f3860
algokelvin-373 <kelvinht373@gmail.com>
2022-04-02 19:43:21 +0700

    [Dart] 13 Operator String

--- DartProgramming/bin/m013_operator/Operator10.dart
+++ DartProgramming/bin/m013_operator/Operator10.dart
@@ -1,5 +1,11 @@
 // Operator String
 
 void main() {
+  String a = "AlgoKelvin";
+  String b = "Dart";
+
+  String c = a + ' learn ' + b + ' programming';
+  print('String c: $c');
+  print('Length c: ${c.length}');
 
 }
