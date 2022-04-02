d46e72fba434bf33cd6be61ce165a92156868415
algokelvin-373 <kelvinht373@gmail.com>
2022-04-02 23:46:56 +0700

    [Dart] 15 Operator ':?' and '??'

--- DartProgramming/bin/m015_decision/Decision06.dart
+++ DartProgramming/bin/m015_decision/Decision06.dart
@@ -1,5 +1,13 @@
 // Ternary Operator ( ?: )
 
-void main() {
+import 'dart:io';
 
+void main() {
+  int number;
+  stdout.write('Input number : ');
+  number = int.parse(stdin.readLineSync()!);
+
+  // Ternary Operator '?:'
+  String dt = number > 0 ? "positive" : "negative";
+  print("Your number input is $dt");
 }
