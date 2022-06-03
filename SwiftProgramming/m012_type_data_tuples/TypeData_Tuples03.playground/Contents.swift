fc3a82c4b12bbe1196beb7d5ff16d2ac45e7104a
algokelvin-373 <kelvinht373@gmail.com>
2022-06-03 20:30:57 +0700

    [Swift] [12] Type Data: Tuples

---
+++ SwiftProgramming/m012_type_data_tuples/TypeData_Tuples03.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+import Cocoa
+
+// Tuples with more than 2 parameter
+
+var dataTuples03 = ("Machine Learning", "Neural Network", "Backpropagation")
+var (nameTuples03, memberTuples03, typeTuples03) = dataTuples03
+
+print("Name Tuples: \(nameTuples03)")
+print("Member Tuples: \(memberTuples03)")
+print("Type Tuples: \(typeTuples03)")
