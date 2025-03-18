84a259f16b31164ae5467a449293f6524dc5e706
algokelvin-373 <kelvinht373@gmail.com>
2025-03-18 23:02:12 +0700

    [Java] Delete Code is not Use

--- JavaProgramming/src/LiveCodeJava/casestudy/MainCaseStudy.java
+++
@@ -1,7 +0,0 @@
-package casestudy;
-
-public class MainCaseStudy {
-    public static void main(String[] args) {
-        System.out.println("Main Case Study");
-    }
-}
--- JavaProgramming/src/LiveCodeJava/material/MainMaterial.java
+++
@@ -1,7 +0,0 @@
-package material;
-
-public class MainMaterial {
-    public static void main(String[] args) {
-        System.out.println("Main Material");
-    }
-}
