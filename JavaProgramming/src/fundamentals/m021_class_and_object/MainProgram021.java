b3cafd4cdae77ea9e2f1e98b9d337548939027ca
algokelvin-373 <kelvinht373@gmail.com>
2025-03-20 00:10:29 +0700

    [Java] Update

---
+++ JavaProgramming/src/fundamentals/m021_class_and_object/MainProgram021.java
@@ -0,0 +1,10 @@
+package fundamentals.m021_class_and_object;
+
+public class MainProgram021 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.brand = "Toyota";
+        car.year = 2020;
+        car.info();
+    }
+}
