28fbe7805854ece13e84746b3b2b348339ee1b64
algokelvin-373 <kelvinht373@gmail.com>
2025-03-29 16:50:41 +0700

    [Python] Day 1 Done

--- PythonProgramming/learning/udemy/practice_udemy/day_001/PrintingProgram.py
+++ PythonProgramming/learning/udemy/practice_udemy/day_001/PrintingProgram.py
@@ -3,3 +3,10 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print('print("what to print?")')
+
+print('')
+print('1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.')
+print('2. Knead the dough for 10 minutes.')
+print('3. Add 3g of Salt.')
+print('4. Leave to rise for 2 hours.')
+print('5. Bake at 200 degrees C for 30 minutes.')
