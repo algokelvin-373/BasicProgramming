2724f29858b5a6e5912f35010c2d69af8ccad3f1
algokelvin-373 <kelvinht373@gmail.com>
2025-04-02 15:39:04 +0700

    [Python] Day 6 Done

---
+++ PythonProgramming/learning/udemy/project_udemy/p006/ReeborgWorld.py
@@ -0,0 +1,52 @@
+# Go to Link Web:
+# https://reeborg.ca/reeborg.html
+
+# Case Study 1
+# def turnLeft():
+#     turn_left()
+#     move()
+#
+# def turnRight():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+#
+# move()
+# move()
+# turnLeft()
+# move()
+# move()
+# turnRight()
+# move()
+
+# Case Study 2 - Area: Hurdle 1
+# def turnLeft():
+#     turn_left()
+#
+# def turnRight():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turnLeft()
+#     move()
+#     turnRight()
+#     move()
+#     turnRight()
+#     move()
+#     turnLeft()
+#
+# move()
+# jump()
+# move()
+# jump()
+# move()
+# jump()
+# move()
+# jump()
+# move()
+# jump()
+# move()
+# jump()
