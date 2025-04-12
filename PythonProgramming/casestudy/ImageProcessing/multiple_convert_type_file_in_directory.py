ddbfcf8d6332c47a375f585dfd98e611c8bde3f2
algokelvin-373 <kelvinht373@gmail.com>
2025-04-12 23:23:05 +0700

    [Python] Upgrade multiple_convert_type_file_in directory - convert_png_to_jpg

--- PythonProgramming/casestudy/ImageProcessing/multiple_convert_type_file_in_directory.py
+++ PythonProgramming/casestudy/ImageProcessing/multiple_convert_type_file_in_directory.py
@@ -2,6 +2,7 @@ import glob
 import os
 
 from casestudy.ImageProcessing.convert_jpeg_to_jpg import convert_jpeg_to_jpg
+from casestudy.ImageProcessing.convert_png_to_jpg import convert_png_to_jpg
 from casestudy.ImageProcessing.convert_webp_to_jpg import convert_webp_to_jpg
 
 def multiple_convert_webp(input_dir, output_dir=None, quality=85):
@@ -34,7 +35,23 @@ def multiple_convert_jpeg(input_dir, output_dir=None, quality=85):
         )
         convert_jpeg_to_jpg(jpeg_file, jpg_file, quality)
 
+def multiple_convert_png(input_dir, output_dir=None, quality=85):
+    if output_dir is None:
+        output_dir = input_dir
+    elif not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    png_files = glob.glob(os.path.join(input_dir, '*.png'))
+
+    for png_file in png_files:
+        jpg_file = os.path.join(
+            output_dir,
+            os.path.splitext(os.path.basename(png_file))[0] + '.jpg'
+        )
+        convert_png_to_jpg(png_file, jpg_file, quality)
+
 input_directory = 'D:/INA17Group/0S_ServerIna17/photos_custom'
 output_directory = 'D:/INA17Group/0S_ServerIna17/photos_result'
 multiple_convert_webp(input_directory, output_directory)
 multiple_convert_jpeg(input_directory, output_directory)
+multiple_convert_png(input_directory, output_directory)
