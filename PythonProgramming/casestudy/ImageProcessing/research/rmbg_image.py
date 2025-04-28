c45e5ca34a60f97b20ccb00ce13820889ea3aca8
algokelvin-373 <kelvinht373@gmail.com>
2025-04-28 15:40:01 +0700

    [Python] Case Study - Segmentation Image

--- PythonProgramming/casestudy/ImageProcessing/research/rmbg_image.py
+++ PythonProgramming/casestudy/ImageProcessing/research/rmbg_image.py
@@ -3,8 +3,9 @@ import time as time
 
 # Read Image
 times = time.time()
-input_path = "D:/test10.jpg"
+input_path = "D:/sample4.jpg"
 output_path = f"D:/output_no_bg_{times}.jpg"
+print(output_path)
 
 # Delete background
 with open(input_path, 'rb') as f:
