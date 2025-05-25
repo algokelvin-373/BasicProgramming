7721db0623212cf3fb63746db6d3eb406d6b910f
algokelvin-373 <kelvinht373@gmail.com>
2025-05-25 20:38:45 +0700

    [Python] Day 20 - Game Snake

---
+++ PythonProgramming/learning/udemy/project_udemy/p020/game_snake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=500, height=500)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.05)
+    snake.move()
+
+
+screen.exitonclick()
