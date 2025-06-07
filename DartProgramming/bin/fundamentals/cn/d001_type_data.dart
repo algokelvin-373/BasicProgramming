2f74b80f56a8604ce057687559718e6ba5ff265a
algokelvin-373 <kelvinht373@gmail.com>
2025-06-07 19:42:30 +0700

    [Dart] 01 - Type Data

---
+++ DartProgramming/bin/fundamentals/cn/d001_type_data.dart
@@ -0,0 +1,14 @@
+void main() {
+  int dataInt = 100;
+  int dataIntInHex = 0xFF;
+  double dataDouble = 3.14;
+  String dataStr = "我是Kelvin。";
+  bool dataBool = true;
+
+  // Show Output
+  print("数据Integer       : " + dataInt.toString());
+  print("数据Integer (Hex) : " + dataIntInHex.toString());
+  print("数据Double        : " + dataDouble.toString());
+  print("数据String        : " + dataStr.toString());
+  print("数据Boolean       : " + dataBool.toString());
+}
