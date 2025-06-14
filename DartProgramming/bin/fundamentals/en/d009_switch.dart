5573bafaf034aef64f4c14129ee5f23041664bbf
algokelvin-373 <kelvinht373@gmail.com>
2025-06-15 01:27:40 +0700

    [Dart 09] Switch

---
+++ DartProgramming/bin/fundamentals/en/d009_switch.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+void main() {
+  print('');
+
+  int number;
+  stdout.write('Input number : ');
+  number = int.parse(stdin.readLineSync()!);
+
+  switch (number) {
+    case 1:
+      print('January');
+      break;
+    case 2:
+      print('February');
+      break;
+    case 3:
+      print('March');
+      break;
+    case 4:
+      print('April');
+      break;
+    case 5:
+      print('May');
+      break;
+    case 6:
+      print('June');
+      break;
+    case 7:
+      print('July');
+      break;
+    case 8:
+      print('August');
+      break;
+    case 9:
+      print('September');
+      break;
+    case 10:
+      print('October');
+      break;
+    case 11:
+      print('November');
+      break;
+    case 12:
+      print('December');
+      break;
+    default:
+      print('No Valid');
+      break;
+  }
+
+  print('');
+}
