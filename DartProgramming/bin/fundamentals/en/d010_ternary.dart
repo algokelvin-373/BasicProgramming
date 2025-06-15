95ef4fb8f43166b9ca6e7754d3f329b5dd4c8e53
algokelvin-373 <kelvinht373@gmail.com>
2025-06-15 11:41:42 +0700

    [Dart 10] Ternary Operator

--- DartProgramming/bin/fundamentals/en/d010_ternary.dart
+++ DartProgramming/bin/fundamentals/en/d010_ternary.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
 
+void main() {
+  print('');
+
+  int number;
+  stdout.write('Input number : ');
+  number = int.parse(stdin.readLineSync()!);
+
+  // Ternary Operator
+  String result = (number > 0) ? "Positive" : "Negative";
+  print(result);
+
+  print('');
+}
