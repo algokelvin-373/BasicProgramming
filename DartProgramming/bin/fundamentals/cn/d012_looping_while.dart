310d31fe708db487ebfdf4bb6de5560db634b232
algokelvin-373 <kelvinht373@gmail.com>
2025-06-20 00:07:52 +0700

    【Dart】 循环 11 - 13

---
+++ DartProgramming/bin/fundamentals/cn/d012_looping_while.dart
@@ -0,0 +1,12 @@
+void main() {
+  print('');
+
+  // 显示数字1-10
+  var i = 1;
+  while (i <= 10) {
+    print("$i ");
+    i++;
+  }
+
+  print('');
+}
---
+++ DartProgramming/bin/fundamentals/cn/d013_looping_do_while.dart
@@ -0,0 +1,12 @@
+void main() {
+  print('');
+
+  // 显示数字1-10
+  var i = 1;
+  do {
+    print('$i ');
+    i++;
+  } while (i <= 10);
+
+  print('');
+}
