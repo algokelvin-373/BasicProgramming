b67c583731b0ef0cfcc663e66b5d1113db9d65ff
algokelvin-373 <kelvinht373@gmail.com>
2025-06-27 21:56:16 +0700

    [Dart]  20 - Function One Expressions

---
+++ DartProgramming/bin/fundamentals/cn/d020_function.dart
@@ -0,0 +1,12 @@
+String showTitle() => 'Dart代码';
+
+String showMyName(String data) => '我是$data';
+
+void main() {
+  print('');
+
+  print(showTitle());
+  print(showMyName('Kelvin'));
+
+  print('');
+}
