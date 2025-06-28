d85e17c43ede54c002375799eee75e7ef21c4aa1
algokelvin-373 <kelvinht373@gmail.com>
2025-06-28 20:53:12 +0700

    [Python] 01 - Type Data

---
+++ PythonProgramming/cn/fundamentals/m001_type_data.py
@@ -0,0 +1,13 @@
+num1 = 4
+num2 = 123456789098765432101234567890
+num3 = 5.6
+num4 = 0.123456789098765432101234567890123456789
+num5 = (3 + 2j)
+bool1 = True
+
+print(f"Value num1 = {num1}, type = ${type(num1)}")
+print(f"Value num2 = {num2}, type = ${type(num2)}")
+print(f"Value num3 = {num3}, type = ${type(num3)}")
+print(f"Value num4 = {num4}, type = ${type(num4)}")
+print(f"Value num5 = {num5}, type = ${type(num5)}")
+print(f"Value bool1 = {bool1}, type = ${type(bool1)}")
