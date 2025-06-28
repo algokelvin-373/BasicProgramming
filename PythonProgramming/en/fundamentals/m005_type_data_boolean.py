c8bff9e5384ef0954fa06666f4b3a6c0b0798dcf
algokelvin-373 <kelvinht373@gmail.com>
2025-06-28 14:00:52 +0700

    [Python] 01 - Type Data

--- PythonProgramming/en/fundamentals/m005_type_data_boolean.py
+++
@@ -1,5 +0,0 @@
-print("Basic Type Data Boolean")
-a = True
-
-# 'bool' is Boolean
-print("Value a = ", a, ", type = ", type(a))
--- PythonProgramming/en/fundamentals/m006_type_data_complex_number.py
+++
@@ -1,4 +0,0 @@
-a = (3 + 2j)    # Type Data Complex Number
-
-# 'complex' is Complex Number
-print("Value a = ", a, ", type = ", type(a))
