c70159604bf1016fb81580a432cb7a9b08a0a514
algokelvin-373 <kelvinht373@gmail.com>
2025-07-03 00:21:05 +0700

    [Python] Case Study General 01 - Modus Data

--- PythonProgramming/en/casestudy/general/csg001_modus_data.py
+++ PythonProgramming/en/casestudy/general/csg001_modus_data.py
@@ -1,12 +1,16 @@
-def find_modus_data(numbers):
-    modus_number = 0
-    for num in set(numbers):
-        if num == numbers.count(num) and num > modus_number:
-            modus_number = num
-    return modus_number
+from collections import Counter
 
 
-result1 = find_modus_data([1, 2, 2, 3, 3, 3])
-result2 = find_modus_data([4, 5, 6, 5, 4, 4, 5, 4, 4, 6])
+def find_modus_data_collection(numbers):
+    if not numbers:     # if numbers is None
+        return -1
+    counts = Counter(numbers)
+    max_count = max(counts.values())
+    modus = [num for num, cnt in counts.items() if cnt == max_count]
+    return modus[0]
+
+
+result1 = find_modus_data_collection([1, 2, 2, 3, 3, 3])
+result2 = find_modus_data_collection([4, 5, 6, 5, 4, 4, 5, 4, 4, 6])
 print(f'Modus for Result1: {result1}')
 print(f'Modus for Result2: {result2}')
