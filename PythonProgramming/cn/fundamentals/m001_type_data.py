26c0f84562f4933f68983441400f27f576cc9708
algokelvin-373 <kelvinht373@gmail.com>
2025-07-03 10:55:14 +0700

    【Python】 01 类型

--- PythonProgramming/cn/fundamentals/m001_type_data.py
+++ PythonProgramming/cn/fundamentals/m001_type_data.py
@@ -7,11 +7,11 @@ num6 = 314e-2   # Exp negative
 num7 = 3e+8     # Exp Positive
 bool1 = True
 
-print(f"Value num1 = {num1}, type = ${type(num1)}")
-print(f"Value num2 = {num2}, type = ${type(num2)}")
-print(f"Value num3 = {num3}, type = ${type(num3)}")
-print(f"Value num4 = {num4}, type = ${type(num4)}")
-print(f"Value num5 = {num5}, type = ${type(num5)}")
-print(f"Value num6 = {num6}, type = ${type(num6)}")
-print(f"Value num7 = {num7}, type = ${type(num7)}")
-print(f"Value bool1 = {bool1}, type = ${type(bool1)}")
+print(f"值 num1 = {num1}, 类型 = ${type(num1)}")
+print(f"值 num2 = {num2}, 类型 = ${type(num2)}")
+print(f"值 num3 = {num3}, 类型 = ${type(num3)}")
+print(f"值 num4 = {num4}, 类型 = ${type(num4)}")
+print(f"值 num5 = {num5}, 类型 = ${type(num5)}")
+print(f"值 num6 = {num6}, 类型 = ${type(num6)}")
+print(f"值 num7 = {num7}, 类型 = ${type(num7)}")
+print(f"值 bool1 = {bool1}, 类型 = ${type(bool1)}")
