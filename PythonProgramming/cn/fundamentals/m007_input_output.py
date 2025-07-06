53d318386eecbe273b3a0127a261f4f9b24af16d
algokelvin-373 <kelvinht373@gmail.com>
2025-07-06 16:10:16 +0700

    [Python] 07 - Update Input Output

--- PythonProgramming/cn/fundamentals/m007_input_output.py
+++ PythonProgramming/cn/fundamentals/m007_input_output.py
@@ -1,14 +1,14 @@
-num1 = input("Input number 1: ")
-num2 = input("Input number 2: ")
+num1 = input("输入数字 1: ")
+num2 = input("输入数字 2: ")
 result = int(num1) + int(num2)
 print(f"{num1} + {num2} = {result}")
 
 print()
 
-name = input("Input your name: ")
-print(f"My name is {name}")
+name = input("输入您的姓名: ")
+print(f"我是{name}")
 
 print()
 
-complex1 = input("Input Complex: ")
-print("Result complex1 = ", eval(complex1))
+complex1 = input("输入复合体: ")
+print("值 complex1 = ", eval(complex1))
