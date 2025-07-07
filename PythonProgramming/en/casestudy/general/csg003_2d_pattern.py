cbe0b41e9c59c8d2c9644d4a71b05622ce934eb0
algokelvin-373 <kelvinht373@gmail.com>
2025-07-07 22:28:13 +0700

    [Python] Case Study  03 - Create Diamond

--- PythonProgramming/en/casestudy/general/csg003_2d_pattern.py
+++ PythonProgramming/en/casestudy/general/csg003_2d_pattern.py
@@ -53,15 +53,60 @@ def rectangle():
         else:
             rectangle_without_hole(width)
 
+def diamond_without_hole(s):
+    for x in range (s):
+        for x1 in range(s-x-1):
+            stdout.write(" ")
+        for x2 in range(2*x+1):
+            stdout.write("*")
+        print()
+    for x in range (s, 1, -1):
+        for x1 in range(s-x+1):
+            stdout.write(" ")
+        for x2 in range(2*x-3):
+            stdout.write("*")
+        print()
+
+def diamond_with_hole(s):
+    for x in range (s):
+        for x1 in range(s-x-1):
+            stdout.write(" ")
+        finish = 2*x+1
+        for x2 in range(finish):
+            if x2 == 0 or x2 == finish-1:
+                stdout.write("*")
+            else:
+                stdout.write(" ")
+        print()
+    for x in range (s, 1, -1):
+        for x1 in range(s-x+1):
+            stdout.write(" ")
+        finish = 2*x-3
+        for x2 in range(finish):
+            if x2 == 0 or x2 == finish-1:
+                stdout.write("*")
+            else:
+                stdout.write(" ")
+        print()
+
+def diamond():
+    s = int(input('Input s: '))
+    hole = input('With Hole (y/n): ')
+    is_hole = (False, True)[hole == 'y']
+    if is_hole:
+        diamond_with_hole(s)
+    else:
+        diamond_without_hole(s)
 
 print('''
 Type 2D:
 1. Triangle
 2. Rectangle
-3. Rhombus
+3. Diamond
 ''')
 type_2d = input('Choose Type 2D: ')
 
 match type_2d:
     case '1': triangle()
     case '2': rectangle()
+    case '3': diamond()
