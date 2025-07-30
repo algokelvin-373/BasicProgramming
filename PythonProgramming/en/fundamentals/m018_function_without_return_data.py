3e6e00b72f766770c03576fba433f11f15dfec4c
algokelvin-373 <kelvinht373@gmail.com>
2025-07-30 22:53:57 +0700

    [Python] 18 - Function without return data

---
+++ PythonProgramming/en/fundamentals/m018_function_without_return_data.py
@@ -0,0 +1,11 @@
+# function general
+def get_begin():
+    print("Python Programming")
+
+# function with parameter
+def get_name(name):
+    print("Your name is ", name)
+
+get_begin()
+data = input("Input your name: ")
+get_name(data)
