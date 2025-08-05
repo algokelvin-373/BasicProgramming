0f7548448c9ddc34c03db575a5d6ccf127ddd205
algokelvin-373 <kelvinht373@gmail.com>
2025-08-05 22:43:29 +0700

    [Python] Update 23 - 24 CN

--- PythonProgramming/en/fundamentals/m023_set.py
+++ PythonProgramming/en/fundamentals/m023_set.py
@@ -1,2 +1,17 @@
-dataSet = {1, 2, 3, 2, 4, 5, 1, 6}  # Set can avoid duplicate data
-print("dataSet = ", dataSet)
+# Set can avoid duplicate data
+
+set1 = {1, 2, 3, 2, 4, 6, 5, 1, 4, 6}
+set2 = {3, 5, 5, 4, 4, 8, 7, 10, 9, 9}
+
+print(f'set1 : {set1}')
+print(f'set2 : {set2}')
+
+print()
+
+print(f'length set1 : {len(set1)}')
+print(f'length set2 : {len(set2)}')
+
+print()
+
+print(f'Intersection set1 && set2 : {set1.intersection(set2)}')
+print(f'Union set1 && set2        : {set1.union(set2)}')
--- PythonProgramming/en/fundamentals/m024_tuple.py
+++ PythonProgramming/en/fundamentals/m024_tuple.py
@@ -1,3 +1,10 @@
-dataTuple = (23, 'Kelvin', 17.5, 2 + 3j)
-print("dataTuple[0] = ", dataTuple[0])
-print("dataTuple[1:3] = ", dataTuple[1:3])
+tuples = (23, 'Kelvin', 17.5, 2 + 3j, 'Python')
+
+print(tuples)
+print(f'length: {len(tuples)}')
+print(f'tuples[1]     : {tuples[1]}')     # index 1
+print(f'tuples[2:4]   : {tuples[2:4]}')   # index 2 until  3
+print(f'tuples[2:]    : {tuples[2:]}')    # index 2 until last
+print(f'tuples[-4]    : {tuples[-4]}')    # index 1
+print(f'tuples[-4:-1] : {tuples[-3:-1]}') # index 2 until  3
+print(f'tuples[-4:]   : {tuples[-3:]}')   # index 2 until last
