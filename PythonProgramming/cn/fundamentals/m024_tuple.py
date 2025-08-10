97fdfb3df0062aff7f4512ee83dd3444078f6851
algokelvin-373 <kelvinht373@gmail.com>
2025-08-10 12:04:04 +0700

    [Python] Update 24 CN

--- PythonProgramming/cn/fundamentals/m024_tuple.py
+++ PythonProgramming/cn/fundamentals/m024_tuple.py
@@ -1,3 +1,9 @@
 dataTuple = (23, 'Kelvin', 17.5, 2 + 3j)
-print("dataTuple[0] = ", dataTuple[0])
-print("dataTuple[1:3] = ", dataTuple[1:3])
+
+print("dataTuple[0] = ", dataTuple[0])      # Index 0
+print("dataTuple[1:3] = ", dataTuple[1:3])  # Index 1 - 3
+print("dataTuple[2:] = ", dataTuple[2:])    # Index 2 - last
+
+print("dataTuple[-4] = ", dataTuple[-4])        # Index 0
+print("dataTuple[-3:-1] = ", dataTuple[-3:-1])  # Index 1 - 3
+print("dataTuple[-2:] = ", dataTuple[-2:])      # Index 2 - last
