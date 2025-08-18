8e01772a9cf5e24b367d387f2fec9684c1feb70c
algokelvin-373 <kelvinht373@gmail.com>
2025-08-18 20:56:35 +0700

    [Python] Case Study - Turtle 00 - Introduction

--- PythonProgramming/en/casestudy/turtles/cst000_introduction.py
+++ PythonProgramming/en/casestudy/turtles/cst000_introduction.py
@@ -1,5 +1,16 @@
 import turtle
 
+turtle.setup(width=400, height=200)
 turtle.color('black')
 turtle.pensize(0)
 
+turtle.penup()      # Not make draw line
+
+turtle.goto(0, 0)
+turtle.write(
+    "Python Code - Turtle",
+        align="center",
+        font=("Arial", 20, "bold")
+)
+turtle.hideturtle()
+turtle.done()
