834f790f814695e6e72b6fa8fca89104237d15f5
algokelvin-373 <kelvinht373@gmail.com>
2025-08-18 21:01:55 +0700

    [Python] Case Study - Turtle 01 - Circle Dimension

--- PythonProgramming/en/casestudy/turtles/cst001_circles_dimension.py
+++ PythonProgramming/en/casestudy/turtles/cst001_circles_dimension.py
@@ -1,12 +1,20 @@
 import turtle
 
+def draw_circle(r):
+    s = r
+    for i in range(10):
+        turtle.up()
+        turtle.goto(0, -r)
+        turtle.down()
+        turtle.circle(r + s)
+        r += s
+
+
+turtle.setup(width=500, height=500)
 turtle.color('black')
 turtle.pensize(0)
 
-r = 20
-for i in range (9):
-    turtle.up()
-    turtle.goto(0, -r)
-    turtle.down()
-    turtle.circle(r + 20)
-    r += 20
+draw_circle(20)
+
+turtle.hideturtle()
+turtle.done()
