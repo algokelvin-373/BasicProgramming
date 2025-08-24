616d8226a137ad75d05ea82b64765232521fe34b
algokelvin-373 <kelvinht373@gmail.com>
2025-08-24 23:45:55 +0700

    [Python] Case Study - Turtle 06 - Radial Squares

---
+++ PythonProgramming/en/casestudy/turtles/cst006_radial_squares.py
@@ -0,0 +1,22 @@
+import turtle
+
+def radial_triangles(side, angle):
+    count = 0
+    while count <= 360:
+        turtle.penup()
+        turtle.pendown()
+        for _ in range(4):
+            turtle.forward(side)
+            turtle.left(90)
+        turtle.left(angle)
+        count += angle
+
+turtle.setup(width=500, height=500)
+turtle.color('black')
+turtle.pensize(0)
+turtle.speed(0)
+
+radial_triangles(100,  1)
+
+turtle.hideturtle()
+turtle.done()
