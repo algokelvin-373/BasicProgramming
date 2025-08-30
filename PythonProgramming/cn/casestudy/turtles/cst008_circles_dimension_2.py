ae6b3de611817cc4b2a71c710e7c9c40389be37d
algokelvin-373 <kelvinht373@gmail.com>
2025-08-30 14:47:58 +0700

    [Python] Python 案例研究 Turtle

---
+++ PythonProgramming/cn/casestudy/turtles/cst008_circles_dimension_2.py
@@ -0,0 +1,21 @@
+import turtle
+
+def radial_circles_dimension(side, dimension):
+    for _ in range(dimension):
+        s = side
+        for _ in range(20):
+            turtle.penup()
+            turtle.pendown()
+            turtle.circle(s)
+            s += 5
+        turtle.left(360 / dimension)
+
+turtle.setup(width=500, height=500)
+turtle.color('black')
+turtle.pensize(1)
+turtle.speed(0)
+
+radial_circles_dimension(20, 6)
+
+turtle.hideturtle()
+turtle.done()
