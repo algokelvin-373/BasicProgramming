18e013753acc19e388449c68e56be8a7d5bb647f
algokelvin-373 <kelvinht373@gmail.com>
2025-09-04 10:43:15 +0700

    [Python] Research 8 - Update

--- PythonProgramming/cn/casestudy/play/graphics/research_8.py
+++ PythonProgramming/cn/casestudy/play/graphics/research_8.py
@@ -1,56 +1,54 @@
 import turtle
 
-turtle.setup(width=500, height=500)
-turtle.color('black')
-turtle.pensize(1)
-turtle.speed(0)
+def circle_design(n, side):
+    if n > 6:
+        turtle.speed(0)
+    else:
+        turtle.speed(10)
+    turtle.penup()
+    turtle.goto(0, 225)
+    turtle.write(f"n = {n}", align="center", font=("Arial", 16, "bold"))
 
-n = 20
-x_start = 0
-y_start = 0
-side = 200
-angle = 360 / n
+    x_start = 0
+    y_start = 0
+    angle = 360 / n
 
-x = []
-y = []
-
-turtle.goto(x_start, y_start)
-for _ in range(n):
-    turtle.forward(side)
-    print(f'x: {turtle.xcor()}')
-    x.append(turtle.xcor())
-    y.append(turtle.ycor())
-    print(f'y: {turtle.ycor()}')
-    turtle.begin_fill()
-    turtle.circle(2)
-    turtle.end_fill()
+    x = []
+    y = []
     turtle.goto(x_start, y_start)
-    turtle.left(angle)
+    turtle.pendown()
+    for _ in range(n):
+        turtle.forward(side)
+        print(f'x: {turtle.xcor()}')
+        x.append(turtle.xcor())
+        y.append(turtle.ycor())
+        print(f'y: {turtle.ycor()}')
+        turtle.begin_fill()
+        turtle.circle(2)
+        turtle.end_fill()
+        turtle.goto(x_start, y_start)
+        turtle.left(angle)
 
-for i in range(len(x)):
-    xs = x[i]
-    ys = y[i]
-    for j in range(len(x)):
-        turtle.goto(xs, ys)
-        xf = x[j]
-        yf = y[j]
-        turtle.goto(xf, yf)
+    for i in range(len(x)):
+        xs = x[i]
+        ys = y[i]
+        for j in range(len(x)):
+            turtle.goto(xs, ys)
+            xf = x[j]
+            yf = y[j]
+            turtle.goto(xf, yf)
 
+    turtle.clear()
 
+turtle.setup(width=500, height=500)
+turtle.bgcolor('black')
+turtle.color('lightblue')
+turtle.pensize(1)
 
-# turtle.goto(x_start, y_start)
-# turtle.forward(side)
-# turtle.begin_fill()
-# turtle.circle(2)
-# turtle.end_fill()
-#
-# turtle.goto(x_start, y_start)
-# turtle.left(180)
-# turtle.forward(side)
-# turtle.begin_fill()
-# turtle.circle(2)
-# turtle.end_fill()
-
+for i in range(20):
+    if i < 2:
+        continue
+    circle_design(i, 200)
 
 turtle.hideturtle()
 turtle.done()
