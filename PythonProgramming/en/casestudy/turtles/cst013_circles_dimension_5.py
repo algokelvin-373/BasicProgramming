b97d0369ebcfb8eb482651526158aae03b3f9975
algokelvin-373 <kelvinht373@gmail.com>
2025-09-07 21:49:26 +0700

    [Python] Case Study - Turtle 13 -Circle Dimension 5

---
+++ PythonProgramming/en/casestudy/turtles/cst013_circles_dimension_5.py
@@ -0,0 +1,32 @@
+import turtle
+
+turtle.setup(width=500, height=500)
+turtle.bgcolor('black')
+turtle.color('lightblue')
+turtle.pensize(1)
+
+side = 100
+for _ in range(4):
+    turtle.begin_fill()
+    turtle.circle(side, 180)
+    turtle.circle(side/2, 180)
+    turtle.circle(-side/2, 180)
+    turtle.end_fill()
+    turtle.left(90)
+
+# turtle.begin_fill()
+# turtle.circle(100, 180)
+# turtle.circle(50, 180)
+# turtle.circle(-50, 180)
+# turtle.end_fill()
+#
+# turtle.left(90)
+#
+# turtle.begin_fill()
+# turtle.circle(100, 180)
+# turtle.circle(50, 180)
+# turtle.circle(-50, 180)
+# turtle.end_fill()
+
+turtle.hideturtle()
+turtle.done()
