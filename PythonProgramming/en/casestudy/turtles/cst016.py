333ddebed48f44eae85c79ee68131ff30ffac0f0
algokelvin-373 <kelvinht373@gmail.com>
2025-09-14 19:37:58 +0700

    [Python] Case Study - Turtle 16

---
+++ PythonProgramming/en/casestudy/turtles/cst016.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.setup(width=500, height=500)
+turtle.bgcolor('black')
+turtle.color('lightblue')
+turtle.pensize(1)
+turtle.speed(0)
+
+for i in range(90):
+    turtle.circle(i * 2)
+    turtle.right(10)
+
+turtle.hideturtle()
+turtle.done()
