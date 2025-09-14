8ea680913b89d4413024279a62b4a8efe94ac7d9
algokelvin-373 <kelvinht373@gmail.com>
2025-09-14 21:41:20 +0700

    [Python] Case Study - Turtle 18

---
+++ PythonProgramming/en/casestudy/turtles/cst018.py
@@ -0,0 +1,83 @@
+import turtle
+
+turtle.setup(width=500, height=500)
+turtle.bgcolor('black')
+turtle.color('lightblue')
+turtle.pensize(2)
+turtle.speed(50)
+
+# square center
+turtle.penup()
+turtle.goto(-100, -100)
+turtle.pendown()
+turtle.begin_fill()
+for _ in range(4):
+    turtle.forward(200)
+    turtle.left(90)
+turtle.end_fill()
+
+# side 1
+x1 = -200
+y1 = 200
+while x1 < -100:
+    turtle.penup()
+    turtle.goto(x1, y1)
+    turtle.pendown()
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    x1 += 10
+while y1 > 100:
+    turtle.penup()
+    turtle.goto(x1, y1)
+    turtle.pendown()
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    y1 -= 10
+
+turtle.right(180)
+
+# Side 2
+x1 = 200
+y1 = -200
+
+while x1 > 100:
+    turtle.penup()
+    turtle.goto(x1, y1)
+    turtle.pendown()
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    x1 -= 10
+while y1 < -100:
+    turtle.penup()
+    turtle.goto(x1, y1)
+    turtle.pendown()
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(300)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    y1 += 10
+
+turtle.hideturtle()
+turtle.done()
