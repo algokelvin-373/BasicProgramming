5f3b2f48e35dcd33c55b35d0aa1feea2dc6adcd6
algokelvin-373 <kelvinht373@gmail.com>
2025-09-21 02:22:35 +0700

    [Python] Play Turtle 23

---
+++ PythonProgramming/cn/casestudy/play/graphics/play23_turtle_design8.py
@@ -0,0 +1,43 @@
+import turtle
+
+def design7(x, y):
+    side = x + y
+
+    turtle.penup()
+    turtle.goto(-x, -y)
+    turtle.pendown()
+    for _ in range(4):
+        turtle.forward(side)
+        turtle.left(90)
+
+    turtle.penup()
+    turtle.goto(-x, -y)
+    turtle.pendown()
+
+    space = 0
+    while space <= side:
+        radius = side - space
+        if space == 0:
+            turtle.begin_fill()
+        turtle.forward(space)
+        turtle.circle(radius, 90)
+        turtle.forward(space)
+        turtle.right(270)
+        turtle.forward(space)
+        turtle.circle(radius, 90)
+        turtle.forward(space)
+        turtle.right(-90)
+        if space == 0:
+            turtle.end_fill()
+        space += 30
+
+turtle.setup(width=500, height=500)
+turtle.bgcolor('black')
+turtle.color('salmon')
+turtle.pensize(2)
+turtle.speed(0)
+
+design7(200, 200)
+
+turtle.hideturtle()
+turtle.done()
