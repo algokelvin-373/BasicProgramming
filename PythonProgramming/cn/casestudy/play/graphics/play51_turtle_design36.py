eb40d13e8e8ac5e280b2d78d6b46d6120d853e9d
algokelvin-373 <kelvinht373@gmail.com>
2025-09-29 00:48:39 +0700

    [Python] Play Turtle 51

---
+++ PythonProgramming/cn/casestudy/play/graphics/play51_turtle_design36.py
@@ -0,0 +1,93 @@
+import turtle
+
+def bezier(t, p0, p1, p2):
+    x = (1-t)**2 * p0[0] + 2*(1-t)*t * p1[0] + t**2 * p2[0]
+    y = (1-t)**2 * p0[1] + 2*(1-t)*t * p1[1] + t**2 * p2[1]
+    return x, y
+
+def curva(p0, pc, p1):
+    for i in range(101):
+        t_bezier = i / 100
+        x, y = bezier(t_bezier, p0, pc, p1)
+        if i == 0:
+            turtle.penup()
+            turtle.goto(x, y)
+            turtle.pendown()
+        else:
+            turtle.goto(x, y)
+
+def step_discrete(point, step=10):
+    x, y = point
+
+    if x > 0:
+        x = max(x - step, 0)
+    elif x < 0:
+        x = min(x + step, 0)
+
+    if y > 0:
+        y = max(y - step, 0)
+    elif y < 0:
+        y = min(y + step, 0)
+
+    return x, y
+
+def design35(x, y):
+    side = x + y
+
+    turtle.penup()
+    turtle.goto(-x, -y)
+    turtle.pendown()
+    for _ in range(4):
+        turtle.forward(side)
+        turtle.left(90)
+
+    turtle.goto(x, y)
+    turtle.goto(x, -y)
+    turtle.goto(-x, y)
+    turtle.goto(-x, 0)
+    turtle.goto(x, 0)
+    turtle.goto(x, y)
+    turtle.goto(0, y)
+    turtle.goto(0, -y)
+
+    ps = [
+        (-x, -y),
+        (0, -y),
+        (x, -y),
+        (x, 0),
+        (x, y),
+        (0, y),
+        (-x, y),
+        (-x, 0)
+    ]
+    pf = [
+        (0, -y),
+        (x, -y),
+        (x, 0),
+        (x, y),
+        (0, y),
+        (-x, y),
+        (-x, 0),
+        (-x, -y)
+    ]
+    pc = (0, 0)
+
+    step = 10
+    for _ in range(int(x/step)):
+        for i in range(len(ps)):
+            curva(ps[i], pc, pf[i])
+
+        for i in range(len(ps)):
+            ps[i] = step_discrete(ps[i])
+            pf[i] = step_discrete(pf[i])
+
+turtle.setup(width=500, height=500)
+turtle.bgcolor('black')
+turtle.color('salmon')
+turtle.pensize(1)
+turtle.speed(0)
+
+design35(200, 200)
+
+turtle.hideturtle()
+turtle.done()
