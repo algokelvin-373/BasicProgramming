88e7b6fec917aee8809480272bd313132097fe8a
algokelvin-373 <kelvinht373@gmail.com>
2025-10-06 23:53:17 +0700

    [Python] Play Turtle 75

---
+++ PythonProgramming/cn/casestudy/play/graphics/play75_turtle_design60.py
@@ -0,0 +1,60 @@
+import math
+import turtle
+
+def ellipse(
+        x_center,
+        y_center,
+        radius_x,
+        radius_y,
+        tilt=0,
+        steps=100
+):
+    turtle.penup()
+    x = radius_x * math.cos(0)
+    y = radius_y * math.sin(0)
+    if tilt != 0:
+        rad_tilt = math.radians(tilt)
+        x_rot = x * math.cos(rad_tilt) - y * math.sin(rad_tilt)
+        y_rot = x * math.sin(rad_tilt) + y * math.cos(rad_tilt)
+        x, y = x_rot, y_rot
+    turtle.goto(x_center + x, y_center + y)
+    turtle.pendown()
+
+    # Draw Ellipse
+    for i in range(1, steps + 1):
+        t = 2 * math.pi * i / steps
+        x = radius_x * math.cos(t)
+        y = radius_y * math.sin(t)
+
+        if tilt != 0:
+            rad_tilt = math.radians(tilt)
+            x_rot = x * math.cos(rad_tilt) - y * math.sin(rad_tilt)
+            y_rot = x * math.sin(rad_tilt) + y * math.cos(rad_tilt)
+            x, y = x_rot, y_rot
+
+        turtle.goto(x_center + x, y_center + y)
+
+def design60(r, angle):
+    for _ in range(int(360/angle)):
+        rt = r
+        for _ in range(3):
+            turtle.penup()
+            turtle.goto(0, 0)
+            turtle.forward(rt)
+            turtle.pendown()
+            xt = turtle.xcor()
+            yt = turtle.ycor()
+            ellipse(xt, yt, rt, rt / 2)
+            rt = rt / 2
+        turtle.right(angle)
+
+turtle.setup(width=500, height=500)
+turtle.bgcolor('black')
+turtle.color('aqua')
+turtle.pensize(2)
+turtle.speed(0)
+
+design60(100, 10)
+
+turtle.hideturtle()
+turtle.done()
