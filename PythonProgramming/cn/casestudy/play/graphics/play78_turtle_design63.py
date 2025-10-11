7ad8f185523b69e4da07c521d7ac89b96852327e
algokelvin-373 <kelvinht373@gmail.com>
2025-10-11 21:15:39 +0700

    [Python] Play Turtle 78

---
+++ PythonProgramming/cn/casestudy/play/graphics/play78_turtle_design63.py
@@ -0,0 +1,27 @@
+import turtle
+
+
+def design62(r, angle):
+    alfa = 180
+    for i in range(int(180/angle)):
+        if i % 2 == 0:
+            alfa = 180
+            turtle.circle(r, alfa)
+        else:
+            alfa = -90
+            turtle.circle(r, alfa)
+            turtle.penup()
+            turtle.goto(0, 0)
+            turtle.pendown()
+        turtle.left(angle)
+
+turtle.setup(width=500, height=500)
+turtle.bgcolor('black')
+turtle.color('aqua')
+turtle.pensize(2)
+turtle.speed(0)
+
+design62(100, 5)
+
+turtle.hideturtle()
+turtle.done()
