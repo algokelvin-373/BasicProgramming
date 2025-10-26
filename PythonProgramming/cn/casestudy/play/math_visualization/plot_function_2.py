734b6d13c839f85e920f4bcbecd0d127e69f6e19
algokelvin-373 <kelvinht373@gmail.com>
2025-10-26 21:22:49 +0700

    [Python] Plot Function 2 - function y = x  (Update)

--- PythonProgramming/cn/casestudy/play/math_visualization/plot_function_2.py
+++ PythonProgramming/cn/casestudy/play/math_visualization/plot_function_2.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def create_plot(x_min, x_max, y_min, y_max):
+def create_plot(f, fx, x_min, x_max, y_min, y_max):
     # Setting range
     ax.set_xlim(x_min, x_max)
     ax.set_ylim(y_min, y_max)
@@ -20,13 +21,28 @@ def create_plot(x_min, x_max, y_min, y_max):
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
 
+    x = np.linspace(x_min, x_max, 400)
+    y = f(x)
+    ax.plot(x, y, color='red', label=fx)
+    ax.legend()
 
 min_x = -10
 max_x = 10
 min_y = -10
 max_y = 10
 
+label = 'y = x'
 fig, ax = plt.subplots(figsize=(8, 8))  # Make figure dan axis
-create_plot(min_x, max_x, min_y, max_y) # Create Plot
-plt.title('X and Y')                    # Title Graphic
+
+# Create Plot
+create_plot(
+    lambda x: x,
+    label,
+    min_x,
+    max_x,
+    min_y,
+    max_y
+)
+
+plt.title(label)                        # Title Graphic
 plt.show()                              # Show
