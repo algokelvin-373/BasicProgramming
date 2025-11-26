9e004fe031bed002a91902e413d613f98055360b
algokelvin-373 <kelvinht373@gmail.com>
2025-11-26 15:05:37 +0700

    [Python] Pandas 04 - Update

--- PythonProgramming/cn/casestudy/play/statistic/play04_pandas.py
+++ PythonProgramming/cn/casestudy/play/statistic/play04_pandas.py
@@ -2,23 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = {
-    'Kategori': ['A', 'B', 'C', 'D'],
+    'Category': ['A', 'B', 'C', 'D'],
     '2022': [15, 20, 12, 25],
     '2023': [18, 22, 16, 20],
     '2024': [10, 25, 14, 15],
 }
 
 df = pd.DataFrame(data)
-
-df = df.set_index('Kategori')
-
+df = df.set_index('Category')
 df.plot(kind='line',
         color=['skyblue', 'salmon', 'lightgreen'],
         linewidth=2)
 
-plt.title("Penjualan per Kategori (2022, 2023, 2024)")
+plt.title("Data per Category (2022, 2023, 2024)")
 plt.xlabel("类别")
-plt.ylabel("Penjualan")
+plt.ylabel("Category")
 plt.xticks(rotation=0)
 plt.legend(title="Year")
 plt.grid(axis='y', linestyle='--', alpha=0.5)
