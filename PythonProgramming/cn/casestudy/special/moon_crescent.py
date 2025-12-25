d6f3d630f0a6dfd01a014067c83442c328e8f9d6
algokelvin-373 <kelvinht373@gmail.com>
2025-12-26 00:16:03 +0700

    [Special] Moon Crescent

---
+++ PythonProgramming/cn/casestudy/special/moon_crescent.py
@@ -0,0 +1,37 @@
+import turtle
+
+bg = "#0b1b2b"      # background color
+moon_color = "yellow"
+
+def draw_crescent(radius):
+    moon = turtle.Turtle()
+    moon.hideturtle()
+    moon.speed(0)
+    moon.pensize(0)
+
+    # Object 1
+    moon.color(moon_color)
+    moon.penup()
+    moon.goto(0, -radius)
+    moon.pendown()
+    moon.begin_fill()
+    moon.circle(radius)
+    moon.end_fill()
+
+    # Object 2
+    moon.color(bg)
+    moon.penup()
+    moon.goto(60, -radius)
+    moon.pendown()
+    moon.begin_fill()
+    moon.circle(radius)
+    moon.end_fill()
+
+
+screen = turtle.Screen()
+screen.setup(500, 500)
+screen.bgcolor(bg)
+
+draw_crescent(150)
+
+turtle.done()
