591ebf7b848fef92fdf0f91803312c64a6967712
algokelvin-373 <kelvinht373@gmail.com>
2026-01-03 12:50:06 +0700

    [Python] Play Turtle 85 - Update

--- PythonProgramming/cn/casestudy/play/graphics/play85_turtle_design70.py
+++ PythonProgramming/cn/casestudy/play/graphics/play85_turtle_design70.py
@@ -1,14 +1,24 @@
 import turtle
 
-colors = ['red', 'blue', 'green', 'magenta']
+colors = ['magenta', 'lightblue', 'yellow'
+          ,'orange', 'lightgreen', 'pink']
 
-def design70(r):
-    for i in range(150):
-        turtle.pencolor(colors[i % 4])
-        turtle.rt(i)
-        turtle.circle(r, i)
-        turtle.fd(i)
-        turtle.rt(90)
+def obj(r):
+    for _ in range(4):
+        turtle.forward(r)
+        turtle.left(90)
+        turtle.forward(r)
+        turtle.circle(r, -90)
+        turtle.left(90)
+
+def design70(r, angle):
+    for i in range(int(90/angle)):
+        r1 = r/2
+        turtle.color(colors[i % 6])
+        for _ in range(10):
+            obj(r1)
+            r1 += 10
+        turtle.left(angle)
 
 turtle.setup(width=500, height=500)
 turtle.bgcolor('black')
@@ -16,7 +26,7 @@ turtle.color('aqua')
 turtle.pensize(1)
 turtle.speed(0)
 
-design70(100)
+design70(150, 10)
 
 turtle.hideturtle()
 turtle.done()
